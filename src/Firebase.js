1899d09773d1dc1278a7dba03bd99ceecc22a07a
shivam <shivamsukija57@gmail.com>
2022-01-31 14:44:35 +0530

    initial commit

---
+++ src/Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD5u7ufDpkhDGQt7aGnw_Ujo0uZA6BV-fU",
+  authDomain: "to-do-app-883b6.firebaseapp.com",
+  projectId: "to-do-app-883b6",
+  storageBucket: "to-do-app-883b6.appspot.com",
+  messagingSenderId: "639217327492",
+  appId: "1:639217327492:web:3e604dcb9ac4486ac451d1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ src/components/todo.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import'./todo.css';
+import { useState } from 'react';
+import { useEffect } from 'react';
+
+
+function Task({task,index,completeTask,removeTask}){
+    return(
+        
+        <div className="task" style={{textDecoration:task.completed?"line-through":""}}
+        >
+        {task.title}
+        <button style ={{background:"red"}} 
+        onClick={()=> removeTask(index)}>X</button>
+        <button onClick={()=> completeTask(index)}>Complete</button>
+       
+        </div>
+    );
+}   
+function CreateTask({addTask}){
+    const[value,setValue]=useState("");
+    const handleSubmit = e =>{
+        e.preventDefault();
+        if(!value)return;
+        addTask(value);
+        setValue("");
+    }
+        return (
+            <form onSubmit ={handleSubmit}>
+                <input 
+                type ="text"
+                className ="input"
+                value={value}
+                placeholder ="Add a new Task"
+                onChange ={e=>setValue(e.target.value)}
+                />
+            </form>
+        );
+    
+}
+function Todo(){
+    const[tasksRemaining,setTasksRemaining]=useState(0);
+    const[tasks,setTasks]=useState([
+        {title:"Grab some Pizzas",
+        completed:true
+    },
+    {
+        title:"Doy our Workout",
+        completed:true
+    },
+    {
+        title:"Hangout with friends",
+        completed:false
+    }
+    ]);
+    useEffect(() => { 
+        setTasksRemaining(tasks.filter(task => !task.completed).length) 
+      });
+    const addTask = title=>{
+        const newTasks= [...tasks,{title,completed:false}];
+        setTasks(newTasks);
+    };
+    const completeTask = index =>{
+        const newTasks =[... tasks];
+        newTasks[index].completed=true;
+        setTasks(newTasks);
+    }
+    const removeTask =index =>{
+        const newTasks =[... tasks];
+        newTasks.splice(index,1);
+        setTasks(newTasks);
+    }
+    return (
+        <div className="todo-container">
+            <div className="header">Pending tasks ({tasksRemaining})</div>
+            
+             <div className="tasks">
+                    {tasks.map((task, index) => (
+                        <Task
+                            task={task}
+                            index={index}
+                            completeTask={completeTask}
+                            removeTask ={removeTask}
+                            key={index}
+                        />
+                    ))}
+                </div>
+                <div className="create-task">
+                    <CreateTask
+                    addTask={addTask} />
+                </div>
+
+        </div>
+    );
+}
+export default Todo;
